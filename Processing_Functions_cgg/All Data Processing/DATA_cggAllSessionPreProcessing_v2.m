4ba6f888eee37899682acbc7ea689cd23dd27da2
Pax Poggi <paxpoggi@gmail.com>
2025-08-20 17:12:24 -0500

    Update DATA_cggAllSessionPreProcessing_v2.m

--- Processing_Functions_cgg/All Data Processing/DATA_cggAllSessionPreProcessing_v2.m
+++ Processing_Functions_cgg/All Data Processing/DATA_cggAllSessionPreProcessing_v2.m
@@ -10,7 +10,7 @@ for sidx=1:length(cfg)
     
     inputfolder=cfg(sidx).inputfolder;
     outdatadir=cfg(sidx).outdatadir;
-    monkey_name=cfg(sidx).Monkey_name;
+    monkey_name=cfg(sidx).Monkey_Name;
 
 [Output,Area_Names] = cgg_proc_NeuralDataPreparation(...
     'inputfolder',cfg(sidx).inputfolder,'outdatadir',cfg(sidx).outdatadir,'monkey_name',monkey_name);
