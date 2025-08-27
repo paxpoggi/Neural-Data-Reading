ddea1c47bcc7ec84c4ea59edc919d8f46d2e5b54
Pax Poggi <paxpoggi@gmail.com>
2025-08-27 14:06:54 -0500

    Update cgg_proc_NeuralDataPreparation.m
    fixed mkdir and debugging code ordering

--- Processing_Functions_cgg/Neural_Data_cgg/cgg_proc_NeuralDataPreparation.m
+++ Processing_Functions_cgg/Neural_Data_cgg/cgg_proc_NeuralDataPreparation.m
@@ -91,18 +91,32 @@ keep_raw = cfg.keep_raw;
 keep_notch = cfg.keep_notch;
 
 %%
+%%
 % Make the Experiment and Session output folder names.
 outdatadir_Experiment=[outdatadir, filesep, ExperimentName];
 outdatadir_SessionName=[outdatadir_Experiment, filesep, SessionName];
+%% debugging code
+fprintf('\n[DBG] outdatadir root: %s\n', outdatadir);
 
-% --- Sanity check for SessionName/ExperimentName ---
+
+outdatadir_Experiment = fullfile(outdatadir, ExperimentName);
+outdatadir_SessionName = fullfile(outdatadir_Experiment, SessionName);
+
+fprintf('[DBG] ExperimentName: "%s"\n', ExperimentName);
+fprintf('[DBG] SessionName:   "%s"\n', SessionName);
+
+
+% Check if the Experiment and Session output folders exist and if they do
+% not, create them.
 if ~exist(outdatadir_Experiment, 'dir')
-    error('Experiment folder does not exist: %s', outdatadir_Experiment);
+    mkdir(outdatadir_Experiment);
+end
+if ~exist(outdatadir_SessionName, 'dir')
+    mkdir(outdatadir_SessionName);
 end
 
-if ~exist(outdatadir_SessionName, 'dir')
-    error('Session folder does not exist: %s\n(Check spelling, underscores, dashes, etc.)', outdatadir_SessionName);
-end
+fprintf('[DBG] Expect Experiment dir: %s (exist=%d)\n', outdatadir_Experiment, exist(outdatadir_Experiment,'dir'));
+fprintf('[DBG] Expect Session   dir: %s (exist=%d)\n', outdatadir_SessionName, exist(outdatadir_SessionName,'dir'));
 
 % Make the Event Information, Frame Information, and Trial Information
 % output folder names.
@@ -121,29 +135,7 @@ outdatadir_TrialInformation=[outdatadir_SessionName, filesep, 'Trial_Information
 % outdatadir_LFP=[outdatadir_Activity, filesep, 'LFP'];
 % outdatadir_Spike=[outdatadir_Activity, filesep, 'Spike'];
 % outdatadir_MUA=[outdatadir_Activity, filesep, 'MUA'];
-%% debugging code
-fprintf('\n[DBG] outdatadir root: %s\n', outdatadir);
-fprintf('[DBG] ExperimentName: "%s"\n', ExperimentName);
-fprintf('[DBG] SessionName:   "%s"\n', SessionName);
 
-outdatadir_Experiment = fullfile(outdatadir, ExperimentName);
-outdatadir_SessionName = fullfile(outdatadir_Experiment, SessionName);
-
-fprintf('[DBG] Expect Experiment dir: %s (exist=%d)\n', outdatadir_Experiment, exist(outdatadir_Experiment,'dir'));
-fprintf('[DBG] Expect Session   dir: %s (exist=%d)\n', outdatadir_SessionName, exist(outdatadir_SessionName,'dir'));
-
-if exist(outdatadir_SessionName,'dir')
-    ls(outdatadir_SessionName); % quick peek to verify it's the one you think
-end
-% Check if the Experiment and Session output folders exist and if they do
-% not, create them.
-if ~exist(outdatadir_Experiment, 'dir')
-    mkdir(outdatadir_Experiment);
-end
-if ~exist(outdatadir_SessionName, 'dir')
-    mkdir(outdatadir_SessionName);
-end
-%%end of debugging code
 % Check if the Event Information, Frame Information, and Trial Information
 % output folders exist and if they do not, create them.
 if ~exist(outdatadir_EventInformation, 'dir')
